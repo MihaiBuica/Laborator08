9a8ac254c8c7c373c5b0ea17da4588a69a2c421e
Mihai Buica <mihai.buica@cti.pub.ro>
2021-05-10 11:52:02 +0300

    1-7 solved

--- labtasks/ChatServiceJmDNS/app/src/main/java/ro/pub/cs/systems/eim/lab08/chatservicejmdns/networkservicediscoveryoperations/NetworkServiceDiscoveryOperations.java
+++ labtasks/ChatServiceJmDNS/app/src/main/java/ro/pub/cs/systems/eim/lab08/chatservicejmdns/networkservicediscoveryoperations/NetworkServiceDiscoveryOperations.java
@@ -180,7 +180,13 @@ public class NetworkServiceDiscoveryOperations {
         }
 	// question 5d
 	// set the title of the activity to the advertised service name
-	// Log name, type and port 
+	// Log name, type and port
+        chatActivity.setTitle(serviceInfo.getName());
+        Log.i(Constants.TAG, "Register service " +
+                serviceInfo.getName() + ":" +
+                serviceInfo.getTypeWithSubtype() + ":" +
+                serviceInfo.getPort()
+        );
     }
 
     public void unregisterNetworkService() {
@@ -198,6 +204,7 @@ public class NetworkServiceDiscoveryOperations {
         chatActivity.setConversations(conversations);
         // question 5d
 	// reset the title to default when not advertising anything
+        chatActivity.setTitle("Chat Service JmDNS");
     }
 
     public void startNetworkServiceDiscovery() {
@@ -205,6 +212,7 @@ public class NetworkServiceDiscoveryOperations {
         if (jmDNS != null && serviceListener != null) {
             jmDNS.addServiceListener(Constants.SERVICE_TYPE, serviceListener);
         }
+
     }
 
     public void stopNetworkServiceDiscovery() {
